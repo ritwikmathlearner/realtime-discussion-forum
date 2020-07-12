cb77162033916b0fee8a54daa835a23565ba8060
ritwikmathlearner <ritwikmathhithub@gmail.com>
2020-07-12 20:13:23 +0530

    Completed database seeding

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,12 +1,25 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Str;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => Str::slug($title),
+        'body' => $faker->sentence,
+        'category_id' => function() {
+            return Category::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -1,12 +1,21 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
+use App\Model\Question;
 use App\Model\Reply;
+use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function () {
+            return Question::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
