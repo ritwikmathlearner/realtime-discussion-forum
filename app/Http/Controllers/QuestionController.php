cb8a3d9bdd652e16090f1919dccf06dc071629d5
ritwikmathlearner <ritwikmathhithub@gmail.com>
2020-07-16 23:35:33 +0530

    Impleted Question CRUD along with QuestionResource

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,84 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\QuestionResource;
 use App\Model\Question;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+//use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 
 class QuestionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
+        return new QuestionResource(Question::create($request->all()));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param Question $question
+     * @return Response
      */
     public function show(Question $question)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
+        if ($question == null) {
+            return \response('Question not found', Response::HTTP_NOT_FOUND);
+        }
+        return new QuestionResource($question);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param Question $question
+     * @return Response
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return \response('Question updated', Response::HTTP_ACCEPTED);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
+     * @param Question $question
+     * @return Response
+     * @throws Exception
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return \response('Question deleted', Response::HTTP_ACCEPTED);
     }
 }
---
+++ app/Http/Resources/QuestionResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'path' => $this->path,
+            'body' => $this->body,
+            'user' => $this->user->name,
+            'created_at' => $this->created_at->diffForHumans()
+        ];
+    }
+}
