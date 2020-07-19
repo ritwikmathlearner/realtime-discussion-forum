2688be6affd1a52ba5fe879cc20319ddd9d9116f
ritwikmathlearner <ritwikmathhithub@gmail.com>
2020-07-19 14:08:10 +0530

    Implemented like and dislike functionality

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,73 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Model\Like;
+use App\Model\Reply;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class LikeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
+     * Remove the specified resource from storage.
      *
      * @param  \App\Model\Like  $like
      * @return \Illuminate\Http\Response
      */
-    public function show(Like $like)
+    public function like(Reply $reply)
     {
-        //
+        $like = $reply->likes()->create([
+            'user_id' => 1
+        ]);
+        return \response('Liked', Response::HTTP_ACCEPTED);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Remove the specified resource from storage.
      *
      * @param  \App\Model\Like  $like
      * @return \Illuminate\Http\Response
      */
-    public function edit(Like $like)
+    public function dislike(Reply $reply)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Like $like)
-    {
-        //
+        $reply->likes()->where('user_id', '=', 1)->first()->delete();
+        return \response('Disliked', Response::HTTP_ACCEPTED);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+Route::post('/{reply}/like', 'LikeController@like');
+Route::delete('/{reply}/like', 'LikeController@dislike');
